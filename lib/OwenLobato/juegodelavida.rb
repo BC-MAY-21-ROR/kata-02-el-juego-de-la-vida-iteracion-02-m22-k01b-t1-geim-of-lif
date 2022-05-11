24cfd92cac73115196e8abd6eae50d5120d498ab
EdwardMD <Edwardmi444@gmail.com>
2022-05-11 17:09:50 -0500

    POO UPDATE
    Co-authors: @OwenLobato
    Co-authors: @Jumaquba

--- lib/OwenLobato/juegodelavida.rb
+++ lib/OwenLobato/juegodelavida.rb
@@ -1,121 +1,123 @@
-def ingreso
-  puts "Elegir ancho del tablero"
-  $height = gets.chomp.to_i
-  
-  puts "Elegir alto del tablero"
-  $width = gets.chomp.to_i
-
-
-end
-
-def random
-  rand(2)
-  # indice == 0 ? '.' : '*'
-end
-
-def create_matriz(a, b)
-  new_matriz = Array.new(a) { Array.new(b) {  } }
-  modify_matriz = Array.new(a) { Array.new(b) {  } }
-
-  
-  print "Generacion 1 \n"
-  print "alto : #{$width}  , ancho : #{$height}\n\n"
-
-  creating_matriz = Array.new(a) { Array.new(b) { random } }
-  creating_matriz.each do |a|
-    a.each do |b|
-      if b == 0
-        print ". "
-      else
-        print "* "
-      end
-    end 
-    print "\n"
+class Main
+  def ingreso
+    puts "Elegir ancho del tablero"
+    $width = gets.chomp.to_i
+    
+    puts "Elegir alto del tablero"
+    $height = gets.chomp.to_i
+    
   end
 
-  creating_matriz.each_with_index.map do |array, index_array|
-    array.each_with_index.map do |celula, index_celula|
-      right = []
-      left = []
-      top = []
-      bottom = []
-      
-      #Check left to right
-      if index_celula.zero?
-        right.push(array[index_celula + 1])
-      elsif index_celula == (array.length - 1)
-        left.push(array[index_celula - 1])
-      else
-        left.push(array[index_celula - 1])
-        right.push(array[index_celula + 1])
-      end
+  def random
+    rand(2)
+    # indice == 0 ? '.' : '*'
+  end
 
-      #Check top
-      if index_array.zero?
-        'none'
-      elsif index_celula.zero?
-        top.push(creating_matriz[index_array - 1][index_celula], creating_matriz[index_array - 1][index_celula + 1])
-      elsif index_celula == array.length - 1
-        top.push(creating_matriz[index_array - 1][index_celula - 1], creating_matriz[index_array - 1][index_celula])
-      else
-        top.push(creating_matriz[index_array - 1][index_celula - 1], creating_matriz[index_array - 1][index_celula],
-          creating_matriz[index_array - 1][index_celula + 1])
-      end
-      
 
-      #Check bottom
-      if index_array == (creating_matriz.length - 1)
-        'none'
-      elsif index_celula.zero?
-        bottom.push(creating_matriz[index_array + 1][index_celula], creating_matriz[index_array + 1][index_celula + 1])
-      elsif index_celula == (array.length - 1)
-        bottom.push(creating_matriz[index_array + 1][index_celula - 1], creating_matriz[index_array + 1][index_celula])
-      else
-        bottom.push(creating_matriz[index_array + 1][index_celula - 1],creating_matriz[index_array + 1][index_celula], creating_matriz[index_array + 1][index_celula + 1])
-      end
-      
-      neighboors=left+right+top+bottom
-      total_neighboors = neighboors.sum
+  def create_matriz(a, b)
+    new_matriz = Array.new(a) { Array.new(b) {  } }
+    modify_matriz = Array.new(a) { Array.new(b) {  } }
 
-      #Rules implementation
-      if celula==1
-        if  total_neighboors<2 || total_neighboors>3
-          new_matriz[index_array][index_celula]=0
-          modify_matriz[index_array][index_celula]="."
-        elsif total_neighboors==2 || total_neighboors==3
-          new_matriz[index_array][index_celula]=1
-          modify_matriz[index_array][index_celula]="*"
-        end
-      else #celula==0
-        if total_neighboors==3
-          new_matriz[index_array][index_celula]=1
-          modify_matriz[index_array][index_celula]="*"
+    
+    print "Generacion 1 \n"
+    print "alto : #{$width}  , ancho : #{$height}\n\n"
+
+    creating_matriz = Array.new(a) { Array.new(b) { random } }
+    creating_matriz.each do |a|
+      a.each do |b|
+        if b == 0
+          print ". "
         else
-          new_matriz[index_array][index_celula]=0
-          modify_matriz[index_array][index_celula]="."
-
+          print "* "
         end
-      end
+      end 
+      print "\n"
+    end
+
+    creating_matriz.each_with_index.map do |array, index_array|
+      array.each_with_index.map do |celula, index_celula|
+        right = []
+        left = []
+        top = []
+        bottom = []
+        
+        #Check left to right
+        if index_celula.zero?
+          right.push(array[index_celula + 1])
+        elsif index_celula == (array.length - 1)
+          left.push(array[index_celula - 1])
+        else
+          left.push(array[index_celula - 1])
+          right.push(array[index_celula + 1])
+        end
+
+        #Check top
+        if index_array.zero?
+          'none'
+        elsif index_celula.zero?
+          top.push(creating_matriz[index_array - 1][index_celula], creating_matriz[index_array - 1][index_celula + 1])
+        elsif index_celula == array.length - 1
+          top.push(creating_matriz[index_array - 1][index_celula - 1], creating_matriz[index_array - 1][index_celula])
+        else
+          top.push(creating_matriz[index_array - 1][index_celula - 1], creating_matriz[index_array - 1][index_celula],
+            creating_matriz[index_array - 1][index_celula + 1])
+        end
+        
+
+        #Check bottom
+        if index_array == (creating_matriz.length - 1)
+          'none'
+        elsif index_celula.zero?
+          bottom.push(creating_matriz[index_array + 1][index_celula], creating_matriz[index_array + 1][index_celula + 1])
+        elsif index_celula == (array.length - 1)
+          bottom.push(creating_matriz[index_array + 1][index_celula - 1], creating_matriz[index_array + 1][index_celula])
+        else
+          bottom.push(creating_matriz[index_array + 1][index_celula - 1],creating_matriz[index_array + 1][index_celula], creating_matriz[index_array + 1][index_celula + 1])
+        end
+        
+        neighboors=left+right+top+bottom
+        total_neighboors = neighboors.sum
+
+        #Rules implementation
+        if celula==1
+          if  total_neighboors<2 || total_neighboors>3
+            new_matriz[index_array][index_celula]=0
+            modify_matriz[index_array][index_celula]="."
+          elsif total_neighboors==2 || total_neighboors==3
+            new_matriz[index_array][index_celula]=1
+            modify_matriz[index_array][index_celula]="*"
+          end
+        else #celula==0
+          if total_neighboors==3
+            new_matriz[index_array][index_celula]=1
+            modify_matriz[index_array][index_celula]="*"
+          else
+            new_matriz[index_array][index_celula]=0
+            modify_matriz[index_array][index_celula]="."
+
+          end
+        end
+    end
+
+
   end
 
 
-end
+  #Print new matriz (Generation 2)s
+    print "Generacion 2 \n"
+    # new_matriz.each do |a|
+    #   print a
+    #   print "\n"
 
 
-#Print new matriz (Generation 2)s
-  print "Generacion 2 \n"
-  # new_matriz.each do |a|
-  #   print a
-  #   print "\n"
-
-
-  # end
-  print "alto : #{$width}  , ancho : #{$height}\n\n"
-  modify_matriz.each do |a|
-    print a.join(" ")
-    print "\n"
+    # end
+    print "alto : #{$height}, ancho : #{$width}  \n\n"
+    modify_matriz.each do |a|
+      print a.join(" ")
+      print "\n"
+    end
   end
 end
-
-ingreso
-create_matriz($width, $height)
+juego = Main.new
+juego.ingreso
+juego.create_matriz($height, $width)
